76bf21deba638b545523195c880ef4bd72f94e95
Sean Wei <me@sean.taipei>
2021-10-02 12:06:42 +0800

    Finish HW2

--- Assignment2-Matrix/matrix.cpp
+++ Assignment2-Matrix/matrix.cpp
@@ -1,3 +1,7 @@
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <unistd.h>
 #include <iostream>
 
 using namespace std;
@@ -11,39 +15,72 @@ using namespace std;
  * to a public server, such as a public GitHub repository or a public web page.
  */
 
+unsigned paral(int N, int K);
+
+unsigned A[800][800];
+
 int main() {
-	int N, chk;
-	clock_t begin, end;
-	double cpu_time_used;
-	unsigned A[800][800];
-	unsigned B[800][800];
-	unsigned C[800][800];
+	timeval beg, end;
+	unsigned chk;
+	float ms;
+	int N;
 
 	cout << "Input the matrix dimension: ";
 	cin >> N;
 
-	/* Init */
 	for (int i=0; i<N; i++)
-		for (int j=0; j<N; j++) {
+		for (int j=0; j<N; j++)
 			A[i][j] = i*N + j;
-			B[i][j] = i*N + j;
-			C[i][j] = 0;
+
+	for (int K=1; K<=16; K++) {
+		gettimeofday(&beg, 0);
+		printf("Multiplying matrices using %d process\n", K);
+
+		chk = paral(N, K);
+
+		gettimeofday(&end, 0);
+		ms = (end.tv_sec - beg.tv_sec) * 1000 + (end.tv_usec - beg.tv_usec) / 1000.;
+		printf("Elapsed time: %.3lf ms, Checksum: %u\n", ms, chk);
+	}
+}
+
+unsigned paral(int N, int K) {
+	unsigned chk = 0;
+	int shmid;
+	int L, R;
+	int *C;
+
+	shmid = shmget(IPC_PRIVATE, 800*800*4, IPC_CREAT | 0600);
+	C = (int *) shmat(shmid, NULL, 0);
+
+	for (int i=0; i<N; i++)
+		for (int j=0; j<N; j++)
+			C[i*800 + j] = 0;
+
+	for (int k=0; k<K; k++) {
+		pid_t pid = fork();
+		if (pid < 0) {
+			exit(-1);
+		} else if (pid == 0) {
+			C = (int *) shmat(shmid, NULL, 0);
+			L = (N+K-1) / K * k;
+			R = (k == K-1) ? N : ((N+K-1) / K * (k+1));
+
+			for (int i=L; i<R; i++)
+				for (int j=0; j<N; j++)
+					for (int k=0; k<N; k++)
+						C[i*800 + j] += A[i][k] * A[k][j];
+
+			_exit(0);
 		}
+	}
 
-	/* Calc */
-	printf("Multiplying matrices using %d process\n", 1);
-	begin = clock();
+	for (int k=0; k<K; k++)
+		wait(NULL);
+
 	for (int i=0; i<N; i++)
 		for (int j=0; j<N; j++)
-			for (int k=0; k<N; k++)
-				C[i][j] += A[i][k] * B[k][j];
+			chk += C[i*800 + j];
 
-	chk = 0;
-	for (int i=0; i<N; i++)
-		for (int j=0; j<N; j++)
-			chk += C[i][j];
-
-	end = clock();
-	cpu_time_used = ((double) (end - begin)) / CLOCKS_PER_SEC;
-	printf("Elapsed time: %.6lf sec, Checksum: %u\n", cpu_time_used, chk);
+	return chk;
 }
