b988bdc3c1fb9052002e65fb223c83b6c685828b
Sean Wei <me@sean.taipei>
2021-12-20 16:14:05 +0800

    Improve demo.sh

--- Assignment6-FUSE/demo.sh
+++ Assignment6-FUSE/demo.sh
@@ -1,52 +1,91 @@
-#! /bin/bash
+#!/usr/bin/env bash
+cd "$(dirname "$1")" || exit 1
 
-SRC_DIR="src"
+if [[ $# -ne 1 ]]; then
+	echo "Usage: $1 ./your-tar-fuse"
+	exit 1
+fi
+
+WD="$(pwd)"
 TESTCASE_DIR="testcase"
 OUTPUT_DIR="output"
 ANSWER_DIR="answer"
 PROGRAM_PATH="$(readlink -f "$1" || echo "$1")"
 
+# Colors
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+BLUE=$'\033[1;34m'
+PURPLE=$'\033[1;35m'
+NC=$'\033[m'
+
 rm -f ${OUTPUT_DIR}/* test.tar
 
+
+### Basic ###
 cp basic/test.tar test.tar
 
 ${PROGRAM_PATH} -f tarfs > /dev/null &
 PROGRAM_PID=$!
 sleep 1
-for i in $( seq 1 4 ); do
-  echo "[1;34m===== basic case ${i} =====[m"
-  bash "./${TESTCASE_DIR}/$i.txt" 2>&1 | sed -e 's/sean/nctuos/g' -e 's#/root/Repo/NCTU-110A-OS/Assignment6-FUSE#/home/nctuos/Documents#' > "${OUTPUT_DIR}/${i}.txt"
-  diff -w "${OUTPUT_DIR}/${i}.txt" "${ANSWER_DIR}/${i}.txt" > /dev/null
-  if [ $? -eq 0 ]; then
-    echo "Your answer is [0;32mcorrect[m"
-    correct_cases="${correct_cases} ${i}"
-  else
-    echo "Your answer is [0;31mwrong[m"
-    wrong_cases="${wrong_cases} ${i}"
-  fi
+USER="$(stat -c '%U' tarfs/* | grep -v root | head -n1)"
+for i in {1..4}; do
+	echo "${BLUE}===== basic case $i =====${NC}"
+
+	bash "./${TESTCASE_DIR}/$i.txt" 2>&1 \
+		| sed -e "s/$USER/nctuos/g" -e "s#$WD#/home/nctuos/Documents#" \
+		> "${OUTPUT_DIR}/${i}.txt"
+
+	diff -w "${OUTPUT_DIR}/${i}.txt" "${ANSWER_DIR}/${i}.txt" > /dev/null
+	if [ $? -eq 0 ]; then
+		echo "Your answer is ${GREEN}correct${NC}"
+		correct_cases="${correct_cases} ${i}"
+	else
+		echo "Your answer is ${RED}wrong${NC}"
+		wrong_cases="${wrong_cases} ${i}"
+	fi
 done
 
 kill ${PROGRAM_PID}
 rm -f test.tar
 
+
+### Bonus ###
 cp bonus/test.tar test.tar
 
 ${PROGRAM_PATH} -f tarfs > /dev/null &
 PROGRAM_PID=$!
 sleep 1
-for i in $( seq 5 6 ); do
-  echo "[1;35m===== bonus case ${i} =====[m"
-  bash "./${TESTCASE_DIR}/$i.txt" 2>&1 | sed 's/sean/nctuos/g' > "${OUTPUT_DIR}/${i}.txt"
-  diff -w "${OUTPUT_DIR}/${i}.txt" "${ANSWER_DIR}/${i}.txt" > /dev/null
-  if [ $? -eq 0 ]; then
-    echo "Your answer is [0;32mcorrect[m"
-    correct_cases="${correct_cases} ${i}"
-  else
-    echo "Your answer is [0;31mwrong[m"
-    wrong_cases="${wrong_cases} ${i}"
-  fi
+USER="$(stat -c '%U' tarfs/* | grep -v root | head -n1)"
+for i in {5..6}; do
+	echo "${PURPLE}===== bonus case $i =====${NC}"
+
+	bash "./$TESTCASE_DIR/$i.txt" 2>&1 \
+		| sed "s/$USER/nctuos/g" \
+		> "$OUTPUT_DIR/$i.txt"
+
+	diff -w "$OUTPUT_DIR/$i.txt" "$ANSWER_DIR/$i.txt" > /dev/null
+	if [ $? -eq 0 ]; then
+		echo "Your answer is ${GREEN}correct${NC}"
+		correct_cases="${correct_cases} ${i}"
+	else
+		echo "Your answer is ${RED}wrong${NC}"
+		wrong_cases="${wrong_cases} ${i}"
+	fi
 done
+
 kill ${PROGRAM_PID}
-echo "[1;33m======= Summary =======[m"
-[ -n "${correct_cases}" ] && echo "[0;32m[Correct][m:${correct_cases}"
-[ -n "${wrong_cases}" ] && echo "[0;31m[ Wrong ][m:${wrong_cases}"
+rm -f test.tar
+
+
+### Summary ###
+echo "${YELLOW}======= Summary =======${NC}"
+
+if [ -n "${correct_cases}" ]; then
+	echo "${GREEN}[Correct]${NC}:${correct_cases}"
+fi
+
+if [ -n "${wrong_cases}" ]; then
+	echo "${RED}[ Wrong ]${NC}:${wrong_cases}"
+fi
